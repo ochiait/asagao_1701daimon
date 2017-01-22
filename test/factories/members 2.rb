2ee87fcdb628fe2de8f5a5875ed271cf9abc94d8
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-01-22 16:13:18 +0900

    DEPRECATION WARNING修正２

--- test/factories/members 2.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :member do
-    sequence(:number) { |n| n + 1 }
-    sequence(:name) { |n| "Taro#{n}" }
-    full_name "Yamada Taro"
-    sequence(:email) { |n| "taro#{n}@example.com" }
-    birthday 30.years.ago
-  end
-end
--- test/models/member_test 2.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class MemberTest < ActiveSupport::TestCase
-  test "factory girl" do
-    member = FactoryGirl.create(:member)
-    assert_equal "Yamada Taro", member.full_name
-  end
-end
